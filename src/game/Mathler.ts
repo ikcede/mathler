83e2631a03db566c6a5c4a7419e201e5e9b72ada
ikcede <1250975+ikcede@users.noreply.github.com>
2024-08-31 16:32:29 -0400

    Set up enough code to run the game

--- src/game/Mathler.ts
+++ src/game/Mathler.ts
@@ -1,4 +1,13 @@
-import GameState from "./GameState";
+import GameState from './GameState';
+
+export const operators = new Set<string>([
+  '+', '-', '*', '/'
+]);
+
+export type GuessValidation = {
+  valid: boolean,
+  error?: string,
+};
 
 /** Creates a new game */
 export const newGame = (options: {}): GameState => {
@@ -20,6 +29,7 @@ export const guess = (guess: string, state: GameState): GameState => {
     return state;
   }
 
+  // TODO: Deal with expression parsing and symbolic equals later
   if (guess === state.solution) {
     state.completed = true;
   }
@@ -30,9 +40,40 @@ export const guess = (guess: string, state: GameState): GameState => {
 }
 
 /** Validate an expression */
-export const validate = (guess: string): boolean | string => {
-  if (guess.length == 6) {
-    return true;
+export const validate = (
+  guess: string,
+  target: number
+): GuessValidation => {
+  if (guess.length !== 6) {
+    return {
+      valid: false,
+      error: 'Invalid input length'
+    };
   }
-  return 'invalid length';
+
+  if (operators.has(guess[0]) || operators.has(guess[5])) {
+    return {
+      valid: false,
+      error: 'Guess cannot start or end with an operator'
+    };
+  }
+
+  try {
+    let value = parseInt(eval(guess));
+    if (value === target) {
+      return {
+        valid: true
+      };
+    }
+
+    return {
+      valid: false,
+      error: 'Guess does not compute to ' + target
+    }
+  } catch {
+    return {
+      valid: false,
+      error: 'Error in expression'
+    };
+  }
 }
